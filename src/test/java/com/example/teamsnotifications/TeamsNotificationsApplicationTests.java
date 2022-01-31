37f9ab7032ffafea1d4a21dbb34915a92966ad4e
Gadibia Daro <55360356+dizsart@users.noreply.github.com>
2022-01-31 17:33:32 +0100

    initial commit

---
+++ src/test/java/com/example/teamsnotifications/TeamsNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamsnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
